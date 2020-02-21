b73acb714526fdb6685180296ee98ae78b1a2390
MiaLearnsToCode <meroimia@gmail.com>
2020-02-21 14:21:32 +0000

    start again

--- chapters/admin.py
+++
@@ -1,9 +0,0 @@
-# Register your models here.
-from django.contrib import admin
-from .models import Chapter, Task, Label
-
-# Register your models here.
-admin.site.register(Chapter)
-admin.site.register(Task)
-admin.site.register(Label)
-
--- chapters/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import ChaptersListView, ChapterDetailView
-from django.urls import path
-
-urlpatterns = [
-    path('chapters', ChaptersListView.as_view()),
-    path('chapters/<int:pk>/', ChapterDetailView.as_view()),
-]
