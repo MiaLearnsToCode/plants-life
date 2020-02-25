e453c694f4ba8f8ff5e0bde121d8bf80d5bd64e0
MiaLearnsToCode <meroimia@gmail.com>
2020-02-25 15:06:32 +0000

    removed build from ignore

---
+++ frontend/build/precache-manifest.df536ae33dfbda112e9faefc6dfd270f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24cc785dc8356ee5afdb75ba6f0ab9b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "265e597f86739be9fc38",
+    "url": "/static/css/2.5dabca3a.chunk.css"
+  },
+  {
+    "revision": "8aa7aa6e8ce9dfac098f",
+    "url": "/static/css/main.c99205e1.chunk.css"
+  },
+  {
+    "revision": "265e597f86739be9fc38",
+    "url": "/static/js/2.6f80cdcc.chunk.js"
+  },
+  {
+    "revision": "4e47723aef56c4c7d3a45ec806795252",
+    "url": "/static/js/2.6f80cdcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aa7aa6e8ce9dfac098f",
+    "url": "/static/js/main.b41da0a4.chunk.js"
+  },
+  {
+    "revision": "ccf1d8c0179706a077aa",
+    "url": "/static/js/runtime-main.3a02ae19.js"
+  }
+]);
